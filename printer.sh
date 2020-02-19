df9ee40db27bcf01d861c1db7ba03d253d538627
darkjedeye <eric@kleincore.co.za>
2020-02-19 21:32:00 +0200

    error correcting

--- printer.sh
+++ printer.sh
@@ -5,7 +5,8 @@ then
 tar zxvf cnijfilter2-5.60-1-deb.tar.gz
 cd cnijfilter2-5.60-1-deb
 cd packages
-sudo apt install gdebi figlet toilet neofetch cowsay fortune
+add-apt-repository ppa:dawidd0811/neofetch -y
+sudo apt install gdebi neofetch cowsay fortune && apt install figlet
 sudo gdebi cnijfilter2_5.60-1_amd64.deb
 echo "neofetch" >> /etc/bash.bashrc
 echo "/usr/games/fortune | /usr/games/cowsay -f tux" >> /etc/bash.bashrc
